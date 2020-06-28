8733fecf1422ceba9e48e5fa94e4f8b405c0cd38
Arttu Siren <arttu.siren@gmail.com>
2020-06-28 16:23:41 +0300

    return 3.1

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+
+const app = express()
+
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+      },
+      {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+      },
+      {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+      },
+      {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+      },
+      {
+        "name": "Ilkka Lipsanen",
+        "number": "+358-60-60606060",
+        "id": 5
+      },
+      {
+        "name": "skkrt",
+        "number": "12313",
+        "id": 7
+      }
+]
+
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Phonebook backend running on port ${PORT}`)
+})
