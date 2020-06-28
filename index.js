11ebf3f4f5e37cdf485da5cef9274880a8820990
Arttu Siren <arttu.siren@gmail.com>
2020-06-28 16:37:25 +0300

    return 3.2

--- index.js
+++ index.js
@@ -7,40 +7,47 @@ let persons = [
         "name": "Arto Hellas",
         "number": "040-123456",
         "id": 1
-      },
-      {
+    },
+    {
         "name": "Ada Lovelace",
         "number": "39-44-5323523",
         "id": 2
-      },
-      {
+    },
+    {
         "name": "Dan Abramov",
         "number": "12-43-234345",
         "id": 3
-      },
-      {
+    },
+    {
         "name": "Mary Poppendieck",
         "number": "39-23-6423122",
         "id": 4
-      },
-      {
+    },
+    {
         "name": "Ilkka Lipsanen",
         "number": "+358-60-60606060",
         "id": 5
-      },
-      {
+    },
+    {
         "name": "skkrt",
         "number": "12313",
         "id": 7
-      }
+    }
 ]
 
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+    res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+    let personsText = `Phonebook has information for ${persons.length} people.`
+    let timeText = new Date().toISOString()
+    let responseText = `${personsText}\n\n${timeText}`
+    res.send(responseText)
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Phonebook backend running on port ${PORT}`)
+    console.log(`Phonebook backend running on port ${PORT}`)
 })
