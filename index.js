236b71f3e207735570227c3d093dbd4173527b87
Arttu Siren <arttu.siren@gmail.com>
2021-03-21 11:44:25 +0200

    Prepare project for heroku

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -7,6 +8,7 @@ morgan.token('body', (req, res) => JSON.stringify(req.body));
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(cors())
 
 let persons = [
     {
@@ -103,7 +105,7 @@ app.get('/info', (req, res) => {
     res.send(responseText)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Phonebook backend running on port ${PORT}`)
 })
