16a1e619eaa6d2776f85b177d83181f215d45a7d
Arttu Siren <arttu.siren@gmail.com>
2021-03-21 12:31:53 +0200

    uibuild

---
+++ build/precache-manifest.17ef880ade29e676cfb3a4eab4898dae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff52b78ef13866c00270e5995374149",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebb3be8bf22dc6eea172",
+    "url": "/static/js/2.4ac893ae.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.4ac893ae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b4e8a8a34b6ca670900b",
+    "url": "/static/js/main.21035748.chunk.js"
+  },
+  {
+    "revision": "c4db98a406b743c9d840",
+    "url": "/static/js/runtime-main.76b77c17.js"
+  }
+]);
