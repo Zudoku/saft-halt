592145b3897b50eebdc63a23f3cfc6b7c236624f
Arttu Siren <arttu.siren@gmail.com>
2021-09-12 11:56:49 +0300

    return 3.12

---
+++ mongo.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+const password = process.argv[2]
+const url = `mongodb+srv://fullstack_mooc_zudoku:${password}@cluster0.hyjhu.mongodb.net/phonenumbers?retryWrites=true&w=majority`
+
+const contactInformationSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+  
+const ContactInformation = mongoose.model('ContactInformation', contactInformationSchema)
+
+const addPhoneNumber = (name, number) => {
+    mongoose.connect(url)
+    
+    const contactInformation = new ContactInformation({
+        name: name,
+        number: number
+    })
+      
+    contactInformation.save().then(response => {
+        console.log(`added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+
+const listPhoneNumbers = () => {
+    mongoose.connect(url)
+
+    ContactInformation.find({}).then(result => {
+        result.forEach(contactInformation => {
+          console.log(`${contactInformation.name} ${contactInformation.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
+
+if (process.argv.length == 3) {
+    listPhoneNumbers()
+} else if (process.argv.length == 5) {
+    const name = process.argv[3]
+    const number = process.argv[4]
+    addPhoneNumber(name, number)
+}
